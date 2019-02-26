88b1f8efdf267b97df6238b09cf4cbe3f273defd
zhangzp <zhangzp@getui.com>
2019-02-26 11:31:52 +0800

    build(all): change build script and modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
+  extends: ['plugin:vue/recommended', '@vue/standard', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
